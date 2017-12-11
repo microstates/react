2bf4e9283cc4353b858af93a82d0c624d409ff62
Taras Mankovski <tarasm@gmail.com>
2017-12-11 10:31:26 -0500

    Initial commit

---
+++ tests/connect.test.jsx
@@ -0,0 +1,8 @@
+import 'jest'
+import * as MS from 'microstates'
+import React from 'react'
+import ReactTestUtils from 'react-dom/test-utils'
+
+import connect from '../src/connect'
+
+describe('connect', () => {})
